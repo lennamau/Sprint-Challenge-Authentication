1c6f8d3f7b609ee54eb0f54a960c3a3685905082
Lenna <lenna@lennamau.com>
2019-03-01 09:58:19 -0700

    registration route completed

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -7,9 +7,23 @@ const jwtKey =
 // quickly see what this file exports
 module.exports = {
   authenticate,
+  generateToken
 };
 
 // implementation details
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: '1d',
+  };
+
+  return jwt.sign(payload, jwtKey, options);
+}
+
 function authenticate(req, res, next) {
   const token = req.get('Authorization');
 
--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,12 @@
 const axios = require('axios');
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate, generateToken } = require('../auth/authenticate');
+
+const db = require('../database/dbConfig')
+
+const bcrypt = require('bcryptjs')
+
+
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -9,7 +15,39 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  // implement user registration
+  let user = req.body
+  const hash = bcrypt.hashSync(user.password, 10); 
+  user.password = hash;
+  if (req.body.username && req.body.password) {
+    db('users')
+      .insert(user)
+      .then(ids => {
+        const id = ids[0]
+        db('users')
+          .where('id', id)
+          .then(user => {
+            const token = generateToken(user);
+            res
+              .status(201)
+              .send(token);
+          })
+          .catch(err => {
+            res
+              .status(500)
+              .json({message: 'Could not register at this time'});
+          })
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({message: 'Could not register at this time'});
+      });
+  }
+  else {
+    res
+      .status(400)
+      .json({message: 'Provide a username and password'});
+  }
 }
 
 function login(req, res) {
